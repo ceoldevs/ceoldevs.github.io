ce480a1cff7bd709368c7135db6babcd0896cd22
smc181002 <meherchaitanya18802@gmail.com>
2023-09-16 16:21:30 +0530

    adding learn page for sharing resources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   // ...
   site: "https://ceoldevs.github.io",
   base: "/",
-  integrations: [
-    tailwind(),
-    sitemap(),
-],
+  integrations: [tailwind(), sitemap(), mdx()],
 });
